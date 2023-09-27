ab1a2d3532615533b2097a4577c5c1b6c6ff2180
Felipe Cardenas <daniel.cardenas03@uptc.edu.co>
2023-09-27 11:28:43 -0500

    actualizacion index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,21 @@
 export default function Index() {
     return (
-        <div>
-            <h1>Gestion de Actas Iglesia Divino Niño</h1>
-        </div>
+        <div className="card text-center">
+            <div className="card-header"> 
+           <h2>Parroquia divino Niño Jesus - Sogamoso</h2>
+         </div> 
+        <div className="row justify-content-center align-items-center">
+        <div className="col-md-3 text-center"> 
+          <img
+            src="./src/images/iglesia.jpg"
+            alt="Imagen"
+            className="img-fluid rounded img-thumbnail mb-3"
+          />
+         Para contactarnos:
+         Correo: pdivinoninojesus@hotmail.com
+         Numero telefonico: 312 5131783
+         </div> 
+         </div>  
+         </div>      
     )
 }
